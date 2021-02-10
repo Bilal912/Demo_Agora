46a1f303e4141abc20f2d03573193267c52da389
Bilal912 <bilalt586@gmail.com>
2021-02-10 12:46:50 +0500

    Bilal

--- EDU_HUNT/app/src/main/java/com/edu_touch/edu_hunt/MainActivity.java
+++ EDU_HUNT/app/src/main/java/com/edu_touch/edu_hunt/MainActivity.java
@@ -2,13 +2,37 @@ package com.edu_touch.edu_hunt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Intent i;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void Login(View view) {
+        i = new Intent(MainActivity.this,Login.class);
+        startActivity(i);
+    }
+
+    public void Signup(View view) {
+        i = new Intent(MainActivity.this,Register.class);
+        startActivity(i);
+    }
+
+    public void avy(View view) {
+        i = new Intent(MainActivity.this,otp.class);
+        startActivity(i);
+    }
+
+    public void phone(View view) {
+        i = new Intent(MainActivity.this,Phoneno.class);
+        startActivity(i);
     }
 }
